2355ffe818f2d9ee270418686abbcdf263267051
Unknown <s013297g@student.staffs.ac.uk>
2018-04-06 18:03:35 +0100

    multiple song loading and menu fixes

--- Dual Step Theta/SongMenu.lua
+++ Dual Step Theta/SongMenu.lua
@@ -1,8 +1,9 @@
 require 'Song'
+
 SongMenu = {songList,
             songDisplay,
             selectedPosition
-            }
+          }
 
 function SongMenu.Init()
   songDirectory = {}
@@ -10,11 +11,14 @@ function SongMenu.Init()
   songMenu.songList = {}
   songMenu.songDisplay = {} --list of 7 songs to be drawn
   songMenu.selectedPosition = 0
+  baseSong = 0
   FileLoad()
   songArt = {}
   jump = 0
-  for i = 1,9 do
-    songArt[i] = love.graphics.newImage('Assets/wipsong'..i..'.png')
+  canJump = true
+  for i = 1, TableCount(songMenu.songList) do
+    songArt[i] = love.graphics.newImage('Assets/'..songMenu.songList[i].artFile)
+    print(i)
   end
   songSelect = love.graphics.newImage('Assets/wipsongselect.png')
   return songMenu
@@ -28,9 +32,17 @@ end
 
 function SongMenu.Draw()
   love.graphics.draw(songSelect,1075,375)
-  for i = 1,9 do
-    love.graphics.draw(songArt[i],1100,150*i-50-jump)
+  --for i = 1,7 do
+  --  love.graphics.draw(songArt[i],1100,150*i-1250-jump*150)
+  --end
+  for i = 1+baseSong,7+baseSong do
+    love.graphics.draw(songArt[i],1100,150*i-200-jump*150)
   end
+  --for i = 1,7 do
+  --  love.graphics.draw(songArt[i],1100,150*i+850-jump*150)
+  --end
+  --love.graphics.print(songMenu.songDisplay[4+jump].songName,900,450)
+  
   --Draw graphics of the 7 songs stored in the songDisplay array
   --draw box with song graphic (songMenu.songDisplay[number].artFile)
   --give each song border
@@ -39,21 +51,39 @@ function SongMenu.Draw()
 end
 
 function SongMenu.Update(dt)
-        print(songMenu.songList[1].songName.."\n"..songMenu.songList[1].artist.."\n"..songMenu.songList[1].audioFile.."\n"..songMenu.songList[1].audioPreview.."\n"..songMenu.songList[1].artFile.."\n"..songMenu.songList[1].difficulty.."\n"..songMenu.songList[1].rating.."\n"..songMenu.songList[1].noteChart.."\n"..songMenu.songList[1].bestScore.."\n"..songMenu.songList[1].previousScore) --lines for debugging, remove these
+
+        --print(songMenu.songList[1].songName.."\n"..songMenu.songList[1].artist.."\n"..songMenu.songList[1].audioFile.."\n"..songMenu.songList[1].audioPreview.."\n"..songMenu.songList[1].artFile.."\n"..songMenu.songList[1].difficulty.."\n"..songMenu.songList[1].rating.."\n"..songMenu.songList[1].noteChart.."\n"..songMenu.songList[1].bestScore.."\n"..songMenu.songList[1].previousScore) --lines for debugging, remove these
   --use self. when wanting to access class varaibles
   --check player input to scroll through the list (update selected position)
   --songs scroll in sets of 3 cause 3 slots used for difficulty settings
   --callScrollThroughMenu
   --play shortAudio for selectedSong
-  timer = timer + dt
-  maxTimer = 0.2
-  if (timer >= maxTimer and love.keyboard.isDown('down')) then
-    jump = jump + 150
-    timer = 0
-  elseif (timer >= maxTimer and love.keyboard.isDown('up')) then
-    jump = jump - 150
-    timer = 0
+  for i = 1,7 do
+    songMenu.songDisplay[i] = songMenu.songList[i+baseSong]
   end
+  
+  if (canJump and love.keyboard.isDown('down')) then
+    jump = jump + 1
+    canJump = false
+    baseSong = baseSong + 1
+  elseif (canJump and love.keyboard.isDown('up')) then
+    jump = jump - 1
+    canJump = false
+    baseSong = baseSong - 1
+  end
+  
+  if (not love.keyboard.isDown('down') and not love.keyboard.isDown('up')) then
+    canJump = true
+  end
+  
+  if (jump > 3) then
+    jump = -3
+  end
+    
+  if (jump < -3) then
+    jump = 3
+  end
+  
   if (love.keyboard.isDown('9')) then --remove this is only here to allow for debugging and getting to the playfield
     SongMenu.SelectSong()
   end
@@ -73,21 +103,21 @@ function FileLoad()
   local fileCount = 1
   for line in love.filesystem.lines("Songs/Directory.txt") do
     table.insert(songDirectory, line)
-    for i = 0, TableCount(songDirectory) -1, 3 do
-      songDataStore = {}
-      for line in love.filesystem.lines("Songs/"..songDirectory[fileCount]) do
-        table.insert(songDataStore, line)
-      end
-      songEasy = Song.New()
-      songMid = Song.New()
-      songHard = Song.New()
-      Song.StoreData(songEasy, 0)
-      Song.StoreData(songMid, 1)
-      Song.StoreData(songHard, 2)
-      songMenu.songList[i] = songEasy
-      songMenu.songList[i+1] = songMid
-      songMenu.songList[i+2] = songHard
-      fileCount = fileCount + 1
+  end
+  for i = 1, TableCount(songDirectory),1  do
+    songDataStore = {}
+    for line in love.filesystem.lines("Songs/"..songDirectory[fileCount]) do
+      table.insert(songDataStore, line)
     end
+    songEasy = Song.New()
+    --songMid = Song.New()
+    --songHard = Song.New()
+    Song.StoreData(songEasy, 0)
+    --Song.StoreData(songMid, 1)
+    --Song.StoreData(songHard, 2)
+    songMenu.songList[i] = songEasy
+    --songMenu.songList[i+1] = songMid
+    --songMenu.songList[i+2] = songHard
+    fileCount = fileCount + 1
   end
 end 
